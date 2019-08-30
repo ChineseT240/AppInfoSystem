3727752391420aca0a624ecd7a765fe7993252e0
baiximing <1683685916@qq.com>
2019-08-30 09:07:32 +0800

    UserLogin

--- AppInfoSystem/src/cn/appsys/controller/UserController.java
+++ AppInfoSystem/src/cn/appsys/controller/UserController.java
@@ -1,7 +1,11 @@
 package cn.appsys.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -12,6 +16,12 @@ public class UserController {
 	public String Login(){
 		System.out.println("进入登陆页面");
 		return "devlogin";
+	}
+	
+	@RequestMapping("/JoinLogin")
+	public String JoinLogin(@RequestParam(value = "devCode") String devCode,@RequestParam(value = "devPassword") String devPassword,HttpSession session){
+		System.err.println("devCode:"+devCode+"  ");
+		return "backendlogin";
 		
 	}
 	
--- AppInfoSystem/src/cn/appsys/dao/user/UserMapper.java
+++ AppInfoSystem/src/cn/appsys/dao/user/UserMapper.java
@@ -1,5 +1,17 @@
 package cn.appsys.dao.user;
 
+import java.util.Random;
+
+import org.apache.ibatis.annotations.Param;
+
+import cn.appsys.pojo.Backend_user;
+
 public interface UserMapper {
 
+	/**
+	 * 登陆的方法
+	 * @return
+	 */
+	public Backend_user Login(@Param("userCode") String userCode,@Param("userPassword") String userPassword);
+	
 }
